cf845eb0389a12c9b66331116d397b8066abd6fb
Christian Welzel <gawain@camlann.de>
2018-12-02 21:38:57 +0100

    Rewrite database code.

--- Classes/Hooks/TceMainProcesscmdmap.php
+++ Classes/Hooks/TceMainProcesscmdmap.php
@@ -13,7 +13,10 @@ namespace TYPO3\CMS\Cal\Hooks;
  * The TYPO3 extension Calendar Base (cal) project - inspiring people to share!
  */
 
+use Doctrine\DBAL\FetchMode;
+use OutOfBoundsException;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
@@ -23,52 +26,56 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
  * @author Mario Matzulla <mario(at)matzullas.de>
  */
 class TceMainProcesscmdmap {
+
 	function processCmdmap_postProcess(&$command, &$table, &$id, &$value, &$tce) {
+		/** @var ConnectionPool $connectionPool */
+		$connectionPool = GeneralUtility::makeInstance(ConnectionPool::class);
+
 		switch ($table) {
 			case 'tx_cal_event' :
-				$select = '*';
-				$table = 'tx_cal_event';
-				$where = 'uid = ' . $id;
-				$result = $GLOBALS ['TYPO3_DB']->exec_SELECTquery ($select, $table, $where);
+				$query = $connectionPool->getConnectionForTable('tx_cal_event');
+				$result = $query->select(['*'],'tx_cal_event', ['uid' => $id]);
+
 				if ($result) {
-					while ($row = $GLOBALS ['TYPO3_DB']->sql_fetch_assoc ($result)) {
-						
+					while ($row = $result->fetch(FetchMode::ASSOCIATIVE)) {
+
 						/* If we're in a workspace, don't notify anyone about the event */
 						if ($row ['pid'] > 0 && !$GLOBALS['BE_USER']->workspace) {
 							/* Check Page TSConfig for a preview page that we should use */
 							$pageIDForPlugin = $this->getPageIDForPlugin($row ['pid']);
-							
+
 							$page = BackendUtility::getRecord ('pages', intval ($pageIDForPlugin), "doktype");
 							if ($page ['doktype'] != 254) {
-								$tx_cal_api = GeneralUtility::makeInstance('TYPO3\\CMS\\Cal\\Controller\\Api');
+								$tx_cal_api = GeneralUtility::makeInstance(\TYPO3\CMS\Cal\Controller\Api::class);
 								$tx_cal_api = &$tx_cal_api->tx_cal_api_without ($pageIDForPlugin);
-								
-								$notificationService = & \TYPO3\CMS\Cal\Utility\Functions::getNotificationService ();
+
+								$notificationService = \TYPO3\CMS\Cal\Utility\Functions::getNotificationService ();
 								if ($command == 'delete') {
 									/* If the deleted event is temporary, reset the MD5 of the parent calendar */
 									if ($row ['isTemp']) {
 										$calendar_id = $row ['calendar_id'];
 										$insertFields = Array (
-												'md5' => '' 
+												'md5' => ''
 										);
-										$GLOBALS ['TYPO3_DB']->exec_UPDATEquery ('tx_cal_calendar', 'uid=' . $calendar_id, $insertFields);
+										$update = $connectionPool->getConnectionForTable('tx_cal_calendar');
+										$update->update('tx_cal_calendar', $insertFields, ['uid', $calendar_id]);
 									}
-									
+
 									/** @var \TYPO3\CMS\Cal\Utility\RecurrenceGenerator $rgc */
-									$rgc = GeneralUtility::makeInstance('TYPO3\\CMS\\Cal\\Utility\\RecurrenceGenerator');
+									$rgc = GeneralUtility::makeInstance(\TYPO3\CMS\Cal\Utility\RecurrenceGenerator::class);
 									$rgc->cleanIndexTableOfUid ($id, $table);
-									
+
 									/* Delete all deviations of the event */
-									$GLOBALS ['TYPO3_DB']->exec_DELETEquery ('tx_cal_event_deviation', 'parentid=' . $id);
+									$delete = $connectionPool->getConnectionForTable('tx_cal_event_deviation');
+									$delete->delete('tx_cal_event_deviation', ['parentid', $id]);
 								} else {
 									$notificationService->notifyOfChanges ($row, array (
-											$command => $value 
+											$command => $value
 									));
 								}
 							}
 						}
 					}
-					$GLOBALS ['TYPO3_DB']->sql_free_result ($result);
 				}
 				break;
 			case 'tx_cal_calendar' :
@@ -76,7 +83,7 @@ class TceMainProcesscmdmap {
 
 				if ($command == 'delete') {
 					/* Using getRecordRaw rather than getRecord since the record has already been deleted. */
-					$calendarRow = BackendUtility::getRecordRaw ('tx_cal_calendar', 'uid=' . $id);
+					$calendarRow = BackendUtility::getRecord ('tx_cal_calendar', $id, '*', '', false);
 					/* If the calendar is an External URL or ICS file, then we need to clean up */
 					if (($calendarRow ['type'] == 1) or ($calendarRow ['type'] == 2)) {
 						$service = new \TYPO3\CMS\Cal\Service\ICalendarService();
@@ -86,10 +93,10 @@ class TceMainProcesscmdmap {
 						$service->deleteSchedulerTask ($id);
 					}
 				}
-				
+
 				if ($command == 'copy') {
 					$newCalendarIds = $tce->copyMappingArray ['tx_cal_calendar'];
-					
+
 					// check if source of copy has a scheduler task attached
 					$calendarRow = BackendUtility::getRecord ('tx_cal_calendar', $id);
 					if ($calendarRow ['schedulerId'] > 0) {
@@ -101,44 +108,42 @@ class TceMainProcesscmdmap {
 					}
 				}
 				break;
-			
+
 			case 'tx_cal_exception_event_group' :
 			case 'tx_cal_exception_event' :
 				if ($command == 'delete') {
-					$select = '*';
-					$where = 'uid = ' . $id;
-					$result = $GLOBALS ['TYPO3_DB']->exec_SELECTquery ($select, $table, $where);
+					$query = $connectionPool->getConnectionForTable($table);
+					$result = $query->select(['*'], $table, ['uid' => $id]);
 					if ($result) {
-						while ($row = $GLOBALS ['TYPO3_DB']->sql_fetch_assoc ($result)) {
-							
+						while ($row = $result->fetch(FetchMode::ASSOCIATIVE)) {
+
 							/* If we're in a workspace, don't notify anyone about the event */
 							if ($row ['pid'] > 0 && !$GLOBALS['BE_USER']->workspace) {
 								/* Check Page TSConfig for a preview page that we should use */
 								$pageIDForPlugin = $this->getPageIDForPlugin($row ['pid']);
-								
+
 								$page = BackendUtility::getRecord ('pages', intval ($pageIDForPlugin), "doktype");
 								if ($page ['doktype'] != 254) {
 									$tx_cal_api = new \TYPO3\CMS\Cal\Controller\Api ();
 									$tx_cal_api = &$tx_cal_api->tx_cal_api_without ($pageIDForPlugin);
-									
-									
+
 									/** @var \TYPO3\CMS\Cal\Utility\RecurrenceGenerator $rgc */
-									$rgc = GeneralUtility::makeInstance('TYPO3\\CMS\\Cal\\Utility\\RecurrenceGenerator');
+									$rgc = GeneralUtility::makeInstance(\TYPO3\CMS\Cal\Utility\RecurrenceGenerator::class);
 									$rgc->cleanIndexTableOfUid ($id, $table);
 								}
 							}
 						}
-						$GLOBALS ['TYPO3_DB']->sql_free_result ($result);
 					}
 				}
 				break;
 				case 'tx_cal_event_deviation' :
 					if ($command == 'delete') {
-						$select = 'tx_cal_event.uid, tx_cal_event.pid';
-						$where = 'tx_cal_index.event_uid = tx_cal_event.uid and tx_cal_index.event_deviation_uid = ' . $id;
-						$result = $GLOBALS ['TYPO3_DB']->exec_SELECTquery ($select, 'tx_cal_index, tx_cal_event', $where);
+						$query = $connectionPool->getQueryBuilderForTable('tx_cal_event_deviation');
+						$result = $query->select(['tx_cal_event.uid', 'tx_cal_event.pid'])->from('tx_cal_event')
+							->join('E', 'tx_cal_index', 'I', 'I.event_uid = E.uid')
+							->where(['I.event_deviation_uid' => $id])->execute();
 						if ($result) {
-							while ($row = $GLOBALS ['TYPO3_DB']->sql_fetch_assoc ($result)) {
+							while ($row = $result->fetch(FetchMode::ASSOCIATIVE)) {
 								$this->reindexEvent($row['uid'], $row['pid']);
 							}
 						}
@@ -146,18 +151,20 @@ class TceMainProcesscmdmap {
 					break;
 		}
 	}
+
 	function processCmdmap_preProcess(&$command, &$table, &$id, &$value, &$tce) {
+		/** @var ConnectionPool $connectionPool */
+		$connectionPool = GeneralUtility::makeInstance(ConnectionPool::class);
+
 		switch ($table) {
 			case 'tx_cal_event' :
 				if ($command == 'delete') {
-					
-					$select = '*';
-					$table = 'tx_cal_event';
-					$where = 'uid = ' . $id;
-					$result = $GLOBALS ['TYPO3_DB']->exec_SELECTquery ($select, $table, $where);
+					$query = $connectionPool->getConnectionForTable('tx_cal_event');
+					$result = $query->select(['*'],'tx_cal_event', ['uid' => $id]);
+
 					if ($result) {
-						while ($row = $GLOBALS ['TYPO3_DB']->sql_fetch_assoc ($result)) {
-							
+						while ($row = $result->fetch(FetchMode::ASSOCIATIVE)) {
+
 							/* If we're in a workspace, don't notify anyone about the event */
 							if ($row ['pid'] > 0 && !$GLOBALS['BE_USER']->workspace) {
 								/* Check Page TSConfig for a preview page that we should use */
@@ -167,13 +174,13 @@ class TceMainProcesscmdmap {
 								} else {
 									$pageIDForPlugin = $row ['pid'];
 								}
-								
+
 								$page = BackendUtility::getRecord ('pages', intval ($pageIDForPlugin), "doktype");
 								if ($page ['doktype'] != 254) {
-									
-									$tx_cal_api = GeneralUtility::makeInstance('TYPO3\\CMS\\Cal\\Controller\\Api');
+
+									$tx_cal_api = GeneralUtility::makeInstance(\TYPO3\CMS\Cal\Controller\Api::class);
 									$tx_cal_api = &$tx_cal_api->tx_cal_api_without ($pageIDForPlugin);
-									
+
 									$notificationService = & \TYPO3\CMS\Cal\Utility\Functions::getNotificationService ();
 									// Need to enforce deletion mode
 									$notificationService->notify ($row, 1);
@@ -183,7 +190,7 @@ class TceMainProcesscmdmap {
 					}
 					// We have to delete the gabriel/scheduler events BEFORE the tx_cal_events and
 					// its related tx_cal_fe_user_event_monitor_mm records are gone
-					
+
 					/* Clean up any pending reminders for this event */
 					$reminderService = &\TYPO3\CMS\Cal\Utility\Functions::getReminderService ();
 					try {
@@ -208,26 +215,26 @@ class TceMainProcesscmdmap {
 				break;
 		}
 	}
-	
+
 	public function reindexEvent($eventUid, $pid) {
 		/* If we're in a workspace, don't notify anyone about the event */
 		if ($pid > 0 && !$GLOBALS['BE_USER']->workspace) {
 			/* Check Page TSConfig for a preview page that we should use */
 			$pageIDForPlugin = $this->getPageIDForPlugin($pid);
-	
+
 			$page = BackendUtility::getRecord ('pages', intval ($pageIDForPlugin), "doktype");
 			if ($page ['doktype'] != 254) {
 				$tx_cal_api = new \TYPO3\CMS\Cal\Controller\Api ();
 				$tx_cal_api = &$tx_cal_api->tx_cal_api_without ($pageIDForPlugin);
-					
-					
+
+
 				/** @var \TYPO3\CMS\Cal\Utility\RecurrenceGenerator $rgc */
-				$rgc = GeneralUtility::makeInstance('TYPO3\\CMS\\Cal\\Utility\\RecurrenceGenerator');
+				$rgc = GeneralUtility::makeInstance(\TYPO3\CMS\Cal\Utility\RecurrenceGenerator::class);
 				$rgc->generateIndexForUid ($eventUid, 'tx_cal_event');
 			}
 		}
 	}
-	
+
 	public function getPageIDForPlugin($pid) {
 		$pageTSConf = BackendUtility::getPagesTSconfig ($pid);
 		if ($pageTSConf ['options.'] ['tx_cal_controller.'] ['pageIDForPlugin']) {
